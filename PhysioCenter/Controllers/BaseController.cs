43c5ef0b32cc955128e3e2b4b89c06eda3161597
Ivan Baev <v4nko@abv.bg>
2022-03-09 22:19:23 +0200

    Layout fixed.

---
+++ PhysioCenter/Controllers/BaseController.cs
@@ -0,0 +1,8 @@
+﻿namespace PhysioCenter.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BaseController : Controller
+    {
+    }
+}
--- PhysioCenter/Controllers/HomeController.cs
+++ PhysioCenter/Controllers/HomeController.cs
@@ -6,7 +6,7 @@
 
     using System.Diagnostics;
 
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
