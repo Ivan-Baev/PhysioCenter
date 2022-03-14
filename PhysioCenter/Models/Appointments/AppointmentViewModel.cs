f9c90b864a73f6d557907408be136bfca23c306d
Ivan Baev <v4nko@abv.bg>
2022-03-14 22:14:04 +0200

    Appointment ViewModels

---
+++ PhysioCenter/Models/Appointments/AppointmentViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace PhysioCenter.Models.Appointments
+{
+    using PhysioCenter.Core.Mappings;
+    using PhysioCenter.Infrastructure.Data.Models;
+
+    using System;
+
+    public class AppointmentViewModel : IMapFrom<Appointment>
+    {
+        public string Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientFirstName { get; set; }
+
+        public string TherapistId { get; set; }
+
+        public string TherapistFullName { get; set; }
+
+        public string ServiceId { get; set; }
+
+        public string ServiceName { get; set; }
+    }
+}
