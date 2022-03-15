523bf38d3b09ff44b5247a2ca122df7ed6fef945
Ivan Baev <v4nko@abv.bg>
2022-03-15 15:34:44 +0200

    Added admin filter with search box

--- PhysioCenter/Controllers/HomeController.cs
+++ PhysioCenter/Controllers/HomeController.cs
@@ -9,11 +9,8 @@
 
     public class HomeController : BaseController
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
---
+++ PhysioCenter/wwwroot/js/search-admin.js
@@ -0,0 +1,46 @@
+﻿function searchFunctionAdmin(e) {
+    var filter, table, tr, td, i, txtValue;
+
+    filter = e.target.value.toUpperCase();
+
+
+    table = document.getElementById("adminTable");
+
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) {
+        td = tr[i];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+//function searchFunctionAdmin(e) {
+//    var filter, tr, td, i, txtValue;
+
+
+//    var filter = e.target.value.toUpperCase();
+//    var rows = document.querySelector("#adminTable tbody").rows;
+
+//    input = document.getElementById("searchAdmin");
+
+//    for (i = 1; i < rows; i++) {
+//        td = rows[i];
+//        if (td) {
+//            txtValue = td.textContent || td.innerText;
+//            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//                tr[i].style.display = "";
+//            } else {
+//                tr[i].style.display = "none";
+//            }
+//        }
+//    }
+//}
+
+document.querySelector('#searchAdmin').addEventListener('keyup', searchFunctionAdmin, false);
