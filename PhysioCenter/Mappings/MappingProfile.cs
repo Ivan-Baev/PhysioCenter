0895cae31f56fb2b3d0c95a471e611607825bffb
Ivan Baev <v4nko@abv.bg>
2022-03-17 17:57:19 +0200

    AppointmentInput model mapping

--- PhysioCenter/Mappings/MappingProfile.cs
+++ PhysioCenter/Mappings/MappingProfile.cs
@@ -3,18 +3,15 @@
     using AutoMapper;
 
     using PhysioCenter.Infrastructure.Data.Models;
+    using PhysioCenter.Models;
     using PhysioCenter.Models.Appointments;
-    using PhysioCenter.Models.SelectLists;
 
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
             CreateMap<Appointment, AppointmentViewModel>().ReverseMap();
-
-            CreateMap<Client, ClientSelectListViewModel>().ReverseMap();
-            CreateMap<Therapist, TherapistSelectListViewModel>().ReverseMap();
-            CreateMap<Service, ServiceSelectListViewModel>().ReverseMap();
+            CreateMap<Appointment, AppointmentInputViewModel>().ReverseMap();
         }
     }
 }
