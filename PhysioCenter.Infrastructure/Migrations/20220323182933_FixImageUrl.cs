3fd900135a96fda63f94663dff5e3e2701f1c15b
Ivan Baev <v4nko@abv.bg>
2022-03-23 20:49:45 +0200

    Small changes in DB to try Google Drive as image system. Reverted

---
+++ PhysioCenter.Infrastructure/Migrations/20220323182933_FixImageUrl.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhysioCenter.Infrastructure.Migrations
+{
+    public partial class FixImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageName",
+                table: "Categories",
+                newName: "ImageUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Categories",
+                newName: "ImageName");
+        }
+    }
+}
