40d557b0b023eb3d07bba6c8d8c299f358d3d8d9
Ivan Baev <v4nko@abv.bg>
2022-04-05 01:21:41 +0300

    custom validation attribute for date

---
+++ PhysioCenter/CustomAttributes/DateTimeParser/AllowedDateTime.cs
@@ -0,0 +1,20 @@
+﻿namespace PhysioCenter.CustomAttributes.DateTimeParser
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AllowedDateTime : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime dt = (DateTime)value;
+            var startDate = DateTime.UtcNow;
+            var endDate = startDate.AddMonths(2).Date;
+            if (dt >= startDate && dt <= endDate && dt.Hour >= 9 && dt.Hour <= 19 && dt.Hour != 13 && dt.Minute == 00 && dt.Second == 00)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage ?? "Please select a valid date from the calendar!");
+        }
+    }
+}
