5c7caa453d69f72709d423d9a5c929dd2b3e7640
Ivan Baev <v4nko@abv.bg>
2022-04-05 01:21:06 +0300

    data validation constants

--- PhysioCenter.Infrastructure/Data/Constants/DataValidations.cs
+++ PhysioCenter.Infrastructure/Data/Constants/DataValidations.cs
@@ -2,26 +2,40 @@
 {
     public static class DataValidations
     {
+        public const int BlogTitleMinLength = 10;
         public const int BlogTitleMaxLength = 100;
+
+        public const int BlogContentMinLength = 200;
         public const int BlogContentMaxLength = 2000;
 
+        public const int CategoryNameMinLength = 10;
         public const int CategoryNameMaxLength = 100;
+
+        public const int CategoryDescriptionMinLength = 100;
         public const int CategoryDescriptionMaxLength = 1000;
 
+        public const int ClientFullNameMinLength = 4;
         public const int ClientFullNameMaxLength = 50;
 
+        public const int NoteContentMinLength = 10;
         public const int NoteContentMaxLength = 200;
 
+        public const int ReviewContentMinLength = 40;
         public const int ReviewContentMaxLength = 400;
 
+        public const int ServiceDescriptionMinLength = 50;
         public const int ServiceDescriptionMaxLength = 500;
 
+        public const int ServiceNameMinLength = 5;
         public const int ServiceNameMaxLength = 100;
 
         public const double ServiceMinPrice = 10;
         public const double ServiceMaxPrice = 70;
 
+        public const int TherapistNameMinLength = 4;
         public const int TherapistNameMaxLength = 100;
+
+        public const int TherapistDescriptionMinLength = 50;
         public const int TherapistDescriptionMaxLength = 1000;
     }
 }
