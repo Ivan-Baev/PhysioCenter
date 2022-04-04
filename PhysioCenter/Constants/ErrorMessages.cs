5009ad07f60725ba4f8ab95d58a7cbbe2c375146
Ivan Baev <v4nko@abv.bg>
2022-04-05 01:21:30 +0300

    error messages constants

---
+++ PhysioCenter/Constants/ErrorMessages.cs
@@ -0,0 +1,29 @@
+﻿namespace PhysioCenter.Constants
+{
+    public class ErrorMessages
+    {
+        public const string BlogTitleLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string BlogContentLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string CategoryNameLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string CategoryDescriptionLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string NoteContentLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string ReviewContentLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string ServiceDescriptionLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string ServiceNameLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string ServicePriceRange = "{0} must be between {1} and {2}!";
+
+        public const string TherapistNameLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string TherapistDescriptionLength = "{0} must be between {2} and {1} characters long!";
+
+        public const string UploadImage = "Please upload an image!";
+    }
+}
