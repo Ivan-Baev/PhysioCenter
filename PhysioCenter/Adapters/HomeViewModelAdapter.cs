ea6d316287e9e6b06adbb3124b7ad73184ae6b50
Ivan Baev <v4nko@abv.bg>
2022-04-04 20:26:40 +0300

    Adapter class to create a complex viewmodel

---
+++ PhysioCenter/Adapters/HomeViewModelAdapter.cs
@@ -0,0 +1,29 @@
+﻿namespace PhysioCenter.Adapters
+{
+    using AutoMapper;
+
+    using PhysioCenter.Infrastructure.Data.Models;
+    using PhysioCenter.Models.Blogs;
+    using PhysioCenter.Models.Home;
+    using PhysioCenter.Models.Reviews;
+
+    public class HomeViewModelAdapter : IHomeViewModelAdapter
+    {
+        private readonly IMapper mapper;
+
+        public HomeViewModelAdapter(
+            IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        public HomeViewModel CreateHomeViewModel(IEnumerable<Blog> blogs, IEnumerable<Review> reviews)
+        {
+            return new HomeViewModel
+            {
+                Blogs = mapper.Map<List<BlogViewModel>>(blogs),
+                Reviews = mapper.Map<List<ReviewViewModel>>(reviews)
+            };
+        }
+    }
+}
---
+++ PhysioCenter/Adapters/IHomeViewModelAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace PhysioCenter.Adapters
+{
+    using PhysioCenter.Infrastructure.Data.Models;
+    using PhysioCenter.Models.Home;
+
+    public interface IHomeViewModelAdapter
+    {
+        HomeViewModel CreateHomeViewModel(IEnumerable<Blog> blogs, IEnumerable<Review> reviews);
+    }
+}
