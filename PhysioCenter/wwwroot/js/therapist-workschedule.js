9054336b0c79c14a560964898fe046a8b25e3f21
Ivan Baev <v4nko@abv.bg>
2022-04-08 17:10:23 +0300

    Added date picker for therapist schedule

---
+++ PhysioCenter/wwwroot/js/therapist-workschedule.js
@@ -0,0 +1,17 @@
+﻿var date = new Date();
+var startDate = new Date(date.setHours(date.getHours() + 1));
+var endDate = new Date(date.setMonth(date.getMonth() + 2))
+
+$(document).ready(function () {
+    $(".datepicker").datetimepicker({
+        format: 'dd/mm/yyyy',
+        clearBtn: true,
+        minuteStep: 60,
+        minView: 2,
+        daysOfWeekDisabled: [0, 6],
+        weekStart: 1,
+        startDate: startDate,
+        endDate: endDate,
+        autoclose: true,
+    })
+})
