0e1a0befcc8e37b021ed24e6c84db1913c64ee94
Ivan Baev <v4nko@abv.bg>
2022-04-08 12:37:38 +0300

    Added error controller and custom error pages.

--- PhysioCenter/Models/ErrorViewModel.cs
+++ PhysioCenter/Models/ErrorViewModel.cs
@@ -2,8 +2,14 @@ namespace PhysioCenter.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public int StatusCode { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string RequestId { get; set; }
+
+        public string RequestPath { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
+
+        public bool ShowRequestPath => !string.IsNullOrEmpty(this.RequestPath);
     }
 }
