6044b0b0aa35cbf60b8c5b50945c171d62246d9f
Ivan Baev <v4nko@abv.bg>
2022-04-12 21:47:57 +0300

    Fixed images in therapist seeder

--- PhysioCenter.Infrastructure/Data/Seeding/CustomSeeders/TherapistsSeeder.cs
+++ PhysioCenter.Infrastructure/Data/Seeding/CustomSeeders/TherapistsSeeder.cs
@@ -36,7 +36,7 @@
                                     Id = new Guid("e927b696-781c-4fe3-a748-777df063c469"),
                                     FullName = "George Marasini",
                                     Description = "George Marasini strongly believes that physical therapy can add life to years, not just years to life. He has a passion for helping people and a get sense of satisfaction when his clients acknowledge improvements they’ve made as a result of the treatment. He believes that every injury and dysfunction is unique and every patient responds differently to treatment. This is why he is passionate about providing holistic approach to achieve the best possible outcomes.",
-                                    ProfileImageUrl = "https://cdn2.iconfinder.com/data/icons/random-outline-3/48/random_14-512.png",
+                                    ProfileImageUrl = "https://res.cloudinary.com/physiocenter/image/upload/v1649788810/PhysioCenter%20Images/avatar-b29c2f856d1a2ae30c3152583b7e9d98_qdeyy8.jpg",
                                     UserId = therapist1.Id,
                                 },
                                 new Therapist
@@ -44,7 +44,7 @@
                                     Id = new Guid("21392157-9603-47fa-88ae-773d4dcaecd3"),
                                     FullName = "Alycia Shavon",
                                     Description = "Alycia Shavon is passionate about helping her clients achieve their goals and get back to activities that are important to them. Her attentive and encouraging approach helps her develop a strong rapport with clients and makes them feel at ease.",
-                                    ProfileImageUrl = "https://cdn2.iconfinder.com/data/icons/random-outline-3/48/random_14-512.png",
+                                    ProfileImageUrl = "https://res.cloudinary.com/physiocenter/image/upload/v1649788937/PhysioCenter%20Images/v3_0152314_crmfng.jpg",
                                     UserId = therapist2.Id,
                                 },
                                 new Therapist
@@ -52,7 +52,7 @@
                                     Id = new Guid("388e1767-5878-444c-9230-d74b6733839a"),
                                     FullName = "Edvin Hester",
                                     Description = "Edvin Hester believes in providing a well-rounded treatment plan to his patients that goes beyond what’s done in the treatment room. He integrates aspects of all of his education, experience as a personal trainer, and skills as a massage therapist into one invaluable rehabilitation service to his clients.",
-                                    ProfileImageUrl = "https://cdn2.iconfinder.com/data/icons/random-outline-3/48/random_14-512.png",
+                                    ProfileImageUrl = "https://res.cloudinary.com/physiocenter/image/upload/v1649788874/PhysioCenter%20Images/v3_0323133_rkx8jk.jpg",
                                     UserId = therapist3.Id,
                                 },
                                 new Therapist
@@ -60,7 +60,7 @@
                                     Id = new Guid("84d337f1-595d-4a88-a42e-2573081f2e5f"),
                                     FullName = "Polly Linnette",
                                     Description = "Polly Linnette believes that each individual client and injury is unique. She uses her skills in manual therapy and exercise prescription to tailor each therapy session to the individual. She also believes in taking an active approach to therapy and encourages her clients to participate and become active members in their recovery.",
-                                    ProfileImageUrl = "https://cdn2.iconfinder.com/data/icons/random-outline-3/48/random_14-512.png",
+                                    ProfileImageUrl = "https://res.cloudinary.com/physiocenter/image/upload/v1649788967/PhysioCenter%20Images/v3_0339721_wp7iri.jpg",
                                     UserId = therapist4.Id,
                                 },
                                 new Therapist
@@ -68,7 +68,7 @@
                                     Id = new Guid("07cf1dca-5e86-4a44-8e22-7b3097d3f139"),
                                     FullName = "Albert Frazier",
                                     Description = "Albert Frazier is focused on developing innovative and individualized programs to rehabilitate people who have sustained a neurological injury, including stroke, brain injury, spinal cord injury, as well as complex orthopedic injuries. At Propel, he pursues his passion for helping people work towards their full potential, no matter their limitations or backgrounds. The capacity for recovery following a neurological injury is his driving force.",
-                                    ProfileImageUrl = "https://cdn2.iconfinder.com/data/icons/random-outline-3/48/random_14-512.png",
+                                    ProfileImageUrl = "https://res.cloudinary.com/physiocenter/image/upload/v1649789065/PhysioCenter%20Images/v2_0687684_qcvdyz.jpg",
                                     UserId = therapist5.Id,
                                 },
                                 new Therapist
@@ -76,7 +76,7 @@
                                     Id = new Guid("1729b7be-1140-423c-8729-c81b36f4abed"),
                                     FullName = "Martha Lyle",
                                     Description = "Martha Lyle is passionate about helping her clients achieve their health and rehabilitation goals. She believes that by becoming more efficient in our day to day movements, we can significantly improve our quality of life. To achieve this, she helps people to increase overall strength and coordination and to incorporate regular exercise into their lifestyle.",
-                                    ProfileImageUrl = "https://cdn2.iconfinder.com/data/icons/random-outline-3/48/random_14-512.png",
+                                    ProfileImageUrl = "https://res.cloudinary.com/physiocenter/image/upload/v1649789004/PhysioCenter%20Images/v2_0843769_g57ur0.jpg",
                                     UserId = therapist6.Id,
                                 },
                             };
